fa3c22d371e8e3997ecb221774bb68b81ed08c35
CatMao1230 <cat841230@gmail.com>
2018-02-22 18:33:55 +0800

    Bissextile year

--- ITSA/60th 平、閏年判定/main.cpp
+++ ITSA/60th 平、閏年判定/main.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    int year;
-    while(cin >> year){
+    int year = 0, n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> year;
         if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){ // 閏年
             cout << "Bissextile Year" << endl;
         }
